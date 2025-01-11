815c0088a91cb255449a347faece97a18e520868
Kiss Bercel <kiss.berci20@gmail.com>
2025-01-11 23:20:21 +0200

    Oldal újratervezése
    Oldal újratervezése

--- resources/views/navbarandfooter/footer.blade.php
+++
@@ -1,9 +0,0 @@
-<style>
-  footer{ background-color: #aa8fff;}
-</style>
-<footer class="text-white">
-  <div class="text-center py-2">
-    <small>&copy; 2024 | Pumpkin Studio</small>
-  </div>
-</footer>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
