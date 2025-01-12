d9a770c614e5dff04eb3e9cdd55d61e94470f62f
Kiss Bercel <109385729+ElectronSama@users.noreply.github.com>
2025-01-12 12:53:44 +0200

    Új adatbázis
    Az újra tervezett adatbázisban a következők találhatóak:
    -Tábla a dolgozók részletes adatairól
    -Tábla az alkalmazás felhasználóiról (Új felhasználó létrehozása funkció)
    -Tábla az eseményekről (Bejelentkezés, kijelentkezés, új dolgozó hozzáadása, új felhasználó létrehozása, esemény létrehozása...)

---
+++ Adatbazis.sql
@@ -0,0 +1,191 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 12, 2025 at 11:44 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `comproller`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `esemenyek`
+--
+
+CREATE TABLE `esemenyek` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `esemeny_neve` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `felhasznalok`
+--
+
+CREATE TABLE `felhasznalok` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `felhasznalonev` varchar(255) NOT NULL,
+  `jelszo` varchar(255) NOT NULL,
+  `szerep` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(2, '2025_01_11_195910_create_nyilvantartas_table', 1),
+(4, '2025_01_11_200950_create_felhasznalok_table', 2),
+(7, '2025_01_11_203534_create_esemenyek_table', 3),
+(8, '2025_01_11_204555_create_sessions_table', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `nyilvantartas`
+--
+
+CREATE TABLE `nyilvantartas` (
+  `DolgozoID` bigint(20) UNSIGNED NOT NULL,
+  `Keresztnev` varchar(255) NOT NULL,
+  `Vezeteknev` varchar(255) NOT NULL,
+  `Szuletesi_datum` varchar(255) NOT NULL,
+  `Anyja_neve` varchar(255) NOT NULL,
+  `Tajszam` varchar(255) NOT NULL,
+  `Adoszam` varchar(255) NOT NULL,
+  `Bankszamlaszam` varchar(255) NOT NULL,
+  `Cim` varchar(255) NOT NULL,
+  `Allampolgarsag` varchar(255) NOT NULL,
+  `Tartozkodasi_hely` varchar(255) NOT NULL,
+  `Szemelyigazolvany_szam` varchar(255) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `Telefonszam` varchar(255) NOT NULL,
+  `Munkakor` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sessions`
+--
+
+CREATE TABLE `sessions` (
+  `id` varchar(255) NOT NULL,
+  `user_id` bigint(20) UNSIGNED DEFAULT NULL,
+  `ip_address` varchar(45) DEFAULT NULL,
+  `user_agent` text DEFAULT NULL,
+  `payload` text NOT NULL,
+  `last_activity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `sessions`
+--
+
+INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
+('IK1AAKtI1dfHadvdicXU4yv73A5b5ph3SnKQ9JKZ', NULL, '::1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoicFVvTWVIU04zODRBaHFqNHhHclVIQWptNnJjSzdURGp6d2RsSVhxdyI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MTY6Imh0dHA6Ly9sb2NhbGhvc3QiO31zOjY6Il9mbGFzaCI7YToyOntzOjM6Im9sZCI7YTowOnt9czozOiJuZXciO2E6MDp7fX19', 1736678228);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `esemenyek`
+--
+ALTER TABLE `esemenyek`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `felhasznalok`
+--
+ALTER TABLE `felhasznalok`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `felhasznalok_felhasznalonev_unique` (`felhasznalonev`),
+  ADD UNIQUE KEY `felhasznalok_szerep_unique` (`szerep`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `nyilvantartas`
+--
+ALTER TABLE `nyilvantartas`
+  ADD PRIMARY KEY (`DolgozoID`);
+
+--
+-- Indexes for table `sessions`
+--
+ALTER TABLE `sessions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `sessions_user_id_index` (`user_id`),
+  ADD KEY `sessions_last_activity_index` (`last_activity`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `esemenyek`
+--
+ALTER TABLE `esemenyek`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `felhasznalok`
+--
+ALTER TABLE `felhasznalok`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `nyilvantartas`
+--
+ALTER TABLE `nyilvantartas`
+  MODIFY `DolgozoID` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
