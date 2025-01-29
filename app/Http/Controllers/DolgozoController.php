2729562a99aab7a9c11942e9bc3d821ad4c4dea6
Papp Gyula <144594449+guts7777@users.noreply.github.com>
2025-01-29 09:36:24 +0100

    Új controller nyilvántartás oldalhoz.

---
+++ app/Http/Controllers/DolgozoController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Dolgozo;
+
+class DolgozoController extends Controller
+{
+    public function destroy($id)
+    {
+        $dolgozo = Dolgozo::find($id);
+
+        if ($dolgozo) 
+        {
+            $dolgozo->delete();
+            return response()->json(['success' => true]);
+        }
+
+        return response()->json(['success' => false, 'message' => 'A dolgozó nem található!'], 404);
+    }
+
+    public function show($id)
+    {
+        $dolgozo = Dolgozo::find($id);
+
+        if ($dolgozo) 
+        {
+            return response()->json([
+                'success' => true,
+                'dolgozo' => $dolgozo,
+            ]);
+        } 
+        else 
+        {
+            return response()->json([
+                'success' => false,
+                'message' => 'Dolgozó nem található.',
+            ]);
+        }
+    }
+
+}
