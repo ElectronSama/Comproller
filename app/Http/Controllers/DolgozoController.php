4bbac4f043fea9bbef067ace65b56960d98a650c
Papp Gyula <144594449+guts7777@users.noreply.github.com>
2025-02-03 20:33:25 +0100

    Új frissités függvény hozzáadása.

--- app/Http/Controllers/DolgozoController.php
+++ app/Http/Controllers/DolgozoController.php
@@ -40,4 +40,22 @@ class DolgozoController extends Controller
         }
     }
 
+    public function update(Request $request) 
+    {
+        $dolgozo = Dolgozo::find($request->id);
+        if (!$dolgozo) 
+        {
+            return response()->json(['success' => false, 'error' => 'Dolgozó nem található!']);
+        }
+    
+        $dolgozo->Vezeteknev = $request->vezeteknev;
+        $dolgozo->Keresztnev = $request->keresztnev;
+        $dolgozo->Email = $request->email;
+        $dolgozo->Telefonszam = $request->telefonszam;
+        $dolgozo->Munkakor = $request->munkakor;
+        $dolgozo->save();
+    
+        return response()->json(['success' => true]);
+    } 
+
 }
