78b3d39a133673cc985f0fc71e855a13fdf28917
Németh Gábor <146082233+NGaborka@users.noreply.github.com>
2025-02-04 12:42:28 +0100

    Update footer.blade.php
    footer fejlesztése

--- resources/views/navbarandfooter/footer.blade.php
+++ resources/views/navbarandfooter/footer.blade.php
@@ -1,14 +1,14 @@
 <style>
-  footer{
-    background-color:#7568ff;
-    border-top:#9d94ff 10px solid;
-    color: #c7d8ff;
-    font-weight: bold;
-  }
+    footer {
+        background-color: lightblue;
+        color: black;
+        font-weight: bold;
+        text-align: center;
+        padding: 10px;
+        width: 100%;
+    }
 </style>
-<footer class="text-white">
-  <div class="text-center py-2">
-    <small>&copy; 2023-25 | Pumpkin Studio</small>
-  </div>
-</footer>
+    <footer>
+        <h3>Comproller 2024/2025</h3>
+    </footer>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
