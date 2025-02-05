163550f827209a4fa3ff91b8f374a0a4d5868d6e
Németh Gábor <146082233+NGaborka@users.noreply.github.com>
2025-02-05 09:38:28 +0100

    Update welcome.blade.php
    bejelentkezés hibakezelése hogy ne csússzon össze a szöveg egymással

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -215,19 +215,32 @@
     <script>
 
         let megnyomva = false;
-        function megnez(passwordId, iconElement) {
-            const passwordField = document.getElementById(passwordId);
-            if (!megnyomva) {
+            function megnez(passwordId, iconElement) {
+        const passwordField = document.getElementById(passwordId);
+        if (megnyomva === false) {
             passwordField.type = 'text';
-            iconElement.src = '{{ asset('kepek/szem_ki.png') }}';
+            iconElement.src = 'szem_ki.png';
             megnyomva = true;
-            } else {
+        } else {
             passwordField.type = 'password';
-            iconElement.src = '{{ asset('kepek/szem_be.png') }}';
+            iconElement.src = 'szem_be.png';
             megnyomva = false;
-            }
         }
+    }
 
+        document.getElementById('loginForm').addEventListener('submit', function(event) {
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password-signin').value;
+        const errorMessage = document.getElementById('error-message');
+        
+        if (username === "" || password === "") {
+            event.preventDefault();
+            errorMessage.textContent = "Töltse ki a felhasználónevet vagy a jelszót!";
+        } else {
+            errorMessage.textContent = "";
+        }
+    });
+        
         function bejelentkezes() 
         {
 
